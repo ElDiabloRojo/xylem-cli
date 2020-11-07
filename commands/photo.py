434a559a24247b6dc42df796cfc85a66ccc647e9
ElDiabloRojo <holdens.uk@googlemail.com>
2020-11-07 21:43:27 +0000

    adding timestamp to img output

--- commands/photo.py
+++ commands/photo.py
@@ -1,5 +1,6 @@
 import click
 import os
+import time
 from picamera import PiCamera
 
 camera = PiCamera()
@@ -8,6 +9,8 @@ camera = PiCamera()
 @click.command()
 @click.option('--img-dir',
               default=lambda: os.environ.get('IMG_DIR', ''))
+@click.option('--file-name',
+              default='xylem-photo-' + time.strftime("%Y%m%d-%H%M%S") + '.jpg')
 def cli(img_dir):
     camera.capture(img_dir + 'image.jpg')
     click.echo('photo: %simage.jpg' % img_dir)
