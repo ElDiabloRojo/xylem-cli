354a4625fc07f80ff3fe6e01be5bdf345fce12f3
ElDiabloRojo <holdens.uk@gmail.com>
2020-11-07 21:37:34 +0000

    ammending img dir

--- commands/photo.py
+++ commands/photo.py
@@ -9,5 +9,5 @@ camera = PiCamera()
 @click.option('--img-dir',
               default=lambda: os.environ.get('IMG_DIR', ''))
 def cli(img_dir):
-    camera.capture(img_dir + '/image.jpg')
-    click.echo('photo: %s/image.jpg' % img_dir)
+    camera.capture(img_dir + 'image.jpg')
+    click.echo('photo: %simage.jpg' % img_dir)
