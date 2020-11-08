75bdde9cb0e2f944da801e54855b875e50136efd
ElDiabloRojo <holdens.uk@gmail.com>
2020-11-08 00:43:04 +0000

    message subcommand started

--- commands/photo.py
+++ commands/photo.py
@@ -2,19 +2,21 @@ import click
 import os
 import time
 import boto3
+import botocore.exceptions
 from picamera import PiCamera
 
 camera = PiCamera()
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
 @click.command()
-@click.option('-i', '--img-dir', type=str,
-              default=lambda: os.environ.get('IMG_DIR', ''))
+@click.option('-i', '--img-dir', type=str, envvar='IMG_DIR',
+	      default='/tmp/', help='local directory to store images')
 @click.option('-f', '--file-name', type=str,
-              default='xylem-photo-' + timestr + '.jpg')
+              default='xylem-photo-' + timestr + '.jpg',
+	      help='output image file name')
 @click.option('-r', '--resolution', type=(int, int), 
-	      default=(1920, 1080)) 
-@click.option('-u', '--upload', is_flag=True)
+	      default=(1920, 1080), help='image resolution, width x height') 
+@click.option('-u', '--upload', is_flag=True, help='enable for upload to s3')
 def cli(img_dir, file_name, resolution, upload):
     camera.resolution = (resolution[0], resolution[1]) 
     camera.capture(img_dir + file_name) 
@@ -24,8 +26,7 @@ def cli(img_dir, file_name, resolution, upload):
 
 
 def upload_to_s3(img_dir, file_name):
-    s3 = boto3.client('s3', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                      aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    s3 = boto3.client('s3') 
     bucket = str('xylem-bucket')
     local_file=img_dir + file_name
 
@@ -39,5 +40,4 @@ def upload_to_s3(img_dir, file_name):
     except NoCredentialsError:
         print("Credentials not available")
         return False
-#[TODO: add cleanup for locally created file]
 
